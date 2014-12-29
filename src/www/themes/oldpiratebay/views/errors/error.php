ff52a11ff8b730b7d1ad54e52a64f742ed07ea3d
Roman Ondráček <Roman3349@users.noreply.github.com>
2014-12-29 17:19:02 +0100

    Update error.php

--- src/www/themes/oldpiratebay/views/errors/error.php
+++ src/www/themes/oldpiratebay/views/errors/error.php
@@ -1,8 +1,8 @@
 <div class="hero-unit thanks">
     <div class="left-dummy" style="text-align: center !important;">
-        <h1>Error <?php echo $data['code']; ?></h1>
+        <h1>Error <?= $data['code']; ?></h1>
         <p>
-            <h2><?php echo nl2br(CHtml::encode($data['message'])); ?></h2>
+            <h2><?= nl2br(CHtml::encode($data['message'])); ?></h2>
         </p>
         <p>
             Something went terribly terribly wrong. Try it again later, friend.
